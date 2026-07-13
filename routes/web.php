a5b1cacaf5bee54e5743c243b48aca7b519da102
morikawa honoka <akahuku29maacha@gmail.com>
2026-07-13 17:36:08 +0900

    feat: 管理画面のお問い合わせ一覧表示（ページネーション7件）の初期実装 #11

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ContactController; // 💡 コントローラーを読み込む
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// ログイン後にリダイレクトされる管理画面トップの仮ルート
-Route::get('/admin', function () {
-    return '管理画面トップページ（ログイン成功！）';
-})->middleware(['auth']); // 💡 ログインしていないと入れないようにガードをかける
+// 管理画面のトップページ（お問い合わせ一覧）のルート
+Route::get('/admin', [ContactController::class, 'index'])->middleware(['auth']);
